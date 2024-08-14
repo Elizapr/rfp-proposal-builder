674b87666d90df777b8493515179b5a44b9a6a15
Eliza Pradhan <pradhaneliza2@gmail.com>
2024-08-14 16:29:56 -0700

    Add Mantine, FontAwesome libraries and its setup for use.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.scss'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   )
 }
 
